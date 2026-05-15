1e415e3c52e016b51087691f8357e41840a02551
Getulio Netto <gtpinheironetto@gmail.com>
2026-05-15 01:05:28 -0500

    rotas criadas

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,29 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('land_page');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/project', function () {
+    return view('project');
+});
+
+Route::get('/homePage', function () {
+    return view('homePage');
+});
+
+Route::get('/payment', function () {
+    return view('payment');
 });
